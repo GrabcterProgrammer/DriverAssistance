c704ad7295b8c570667ea585dfeb55fa240d60ee
PingWin <grabcterplay@gmail.com>
2021-01-31 08:38:26 +1000

    Add docs in file

--- DetectionRoad2.py
+++ DetectionRoad2.py
@@ -1,27 +1,28 @@
 import cv2
 import numpy as np
 
-# Setting
-img_size = [400, 200]
+# Настройки
+img_size = [400, 200]  # Размер изображения weight, height
 area_setting = np.float32([
     [40, 180],  # x1 + y1 - Нижняя левая точка
     [390, 180],  # x2 + y2 - Нижняя правая точка
     [280, 100],  # x3 + y3 - Верхняя правая точка
     [120, 100]    # x4 + y4 - Верхняя левая точка
-])
+])  # координаты для рисования прямоугольника
 cords = np.float32([
     [0, img_size[1]],
     [img_size[0], img_size[1]],
     [img_size[0], 1],
     [0, 0]
 ])
-draw_area = np.array(area_setting, dtype=np.int32)
-nwindow = 12
+draw_area = np.array(area_setting, dtype=np.int32)  # Рисования поля
+nwindow = 12  # Кол-во окон для распознования разметки
 
-video = cv2.VideoCapture("video/DriveOnRoad.mp4")
+video = cv2.VideoCapture("video/DriveOnRoad.mp4")  # Чтение видео
 
 
-# function
+# Бинаризация изображения
+# Приводим темные участки к 0, а светлые к 1
 def binary(img, value):
     channel = img[:, :, 2]
     bin = np.zeros_like(channel)
@@ -31,38 +32,40 @@ def binary(img, value):
 
 
 while video.isOpened():
-    _, frame = video.read()
-    frame = cv2.resize(frame, (img_size[0], img_size[1]))
-    frame_hls = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
+    _, frame = video.read()  # Чтение видео
+    frame = cv2.resize(frame, (img_size[0], img_size[1]))  # Изминения размера, для быстроты алгоритма
+    frame_hls = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)  # Перевод в формат hsl
 
     # Бинарезуем изображение
     binary_frame = binary(frame, 200)
     binary_hls = binary(frame_hls, 160)
 
+    # Объединение двух бинаризуемых изображений
     all_binary = np.zeros_like(binary_frame)
     all_binary[((binary_frame == 255) | (binary_hls == 255))] = 255
 
-    # crop video
+    # Рисование прямоугольника
     all_binary_visual = all_binary.copy()
     cv2.polylines(all_binary_visual, [draw_area], True, 255)
     M = cv2.getPerspectiveTransform(area_setting, cords)
     warped = cv2.warpPerspective(all_binary, M, (img_size[0], img_size[1]), flags=cv2.INTER_LINEAR)
 
-    # search while pix
+    # Поиск белых пикселей
     warped_visual = warped.copy()
     histogram = np.sum(warped[warped.shape[0]//2:, :], axis=0)
-    midpoint = histogram.shape[0]//2
-    left_column = np.argmax(histogram[:midpoint])
-    right_column = np.argmax(histogram[midpoint:]) + midpoint
+    midpoint = histogram.shape[0]//2  # нахождение центральной точки
+    left_column = np.argmax(histogram[:midpoint])  # нахождение левой колонки
+    right_column = np.argmax(histogram[midpoint:]) + midpoint  # нахождение правой колонки
+    # Отображение линий
     cv2.line(warped_visual, (left_column, 0), (left_column, warped_visual.shape[0]), 110, 2)
     cv2.line(warped_visual, (right_column, 0), (right_column, warped_visual.shape[0]), 110, 2)
 
-    # search lines in windows
+    # Поиск беблых линий в окне
     nonzero = warped.nonzero()
     WhitePixelIndY = np.array(nonzero[0])
     WhitePixelIndX = np.array(nonzero[1])
 
-    # draw windows on white lines
+    # Рисование окон на линиях
     window_height = np.int(warped.shape[0]/nwindow)
     window_half_width = 25
 
@@ -86,7 +89,7 @@ while video.isOpened():
         cv2.rectangle(out_img, (left_win_x1, win_y1), (left_win_x2, win_y2), (50 + window * 21, 0, 0), 2)
         cv2.rectangle(out_img, (right_win_x1, win_y1), (right_win_x2, win_y2), (0, 0, 50 + window * 21), 2)
 
-        # search lines in windows
+        # Поиск линий в окне
         good_left_inds = ((WhitePixelIndY>=win_y1) & (WhitePixelIndY<=win_y2) & (WhitePixelIndX>=left_win_x1) & (WhitePixelIndX<=left_win_x2)).nonzero()[0]
         good_right_inds = ((WhitePixelIndY >= win_y1) & (WhitePixelIndY <= win_y2) & (WhitePixelIndX >= right_win_x1) & (
                     WhitePixelIndX <= right_win_x2)).nonzero()[0]
@@ -103,21 +106,6 @@ while video.isOpened():
     # search lines in windows
     out_img[(WhitePixelIndY[left_line_inds], WhitePixelIndX[left_line_inds])] = [255, 0, 0]
     out_img[(WhitePixelIndY[right_line_inds], WhitePixelIndX[right_line_inds])] = [0, 0, 255]
-
-    # search central lines
-    # leftx = WhitePixelIndX[left_line_inds]
-    # lefty = WhitePixelIndY[left_line_inds]
-    # rightx = WhitePixelIndX[right_line_inds]
-    # righty = WhitePixelIndY[right_line_inds]
-    #
-    # left_fit = np.polyfit(leftx, lefty, 2)
-    # right_fit = np.polyfit(rightx, righty, 2)
-    #
-    # center_fit = (left_fit + right_fit) / 2
-    #
-    # for ver_ind in range(out_img.shape[0]):
-    #     gor_ind = (center_fit[0]) * (ver_ind **2) + center_fit[1] * ver_ind + center_fit[2]
-    #     cv2.circle(out_img, (int(gor_ind), int(ver_ind)), 2, (255, 0, 255), 2)
 
     # Вывод изображений
     cv2.imshow("Main Video", frame)
